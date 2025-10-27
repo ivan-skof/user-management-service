d1e0985351d77b496980e3bc1ee20682362ebf25
Ivan <ivan.skof@gmail.com>
2025-10-27 12:53:22 +0100

    removed obsolete files

--- UserManagementService.Services/Interfaces/ILogService.cs
+++
@@ -1,7 +0,0 @@
-namespace UserManagementService.Services.Interfaces
-{
-    public interface ILogService
-    {
-        Task LogAsync(string level, string methodName, string requestParams, string message, string clientIp, string clientName);
-    }
-}
