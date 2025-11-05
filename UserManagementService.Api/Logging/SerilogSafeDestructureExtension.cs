ee4304f650c264071e51fd6f266ddbcc2e7c470c
Ivan <ivan.skof@gmail.com>
2025-11-05 12:04:28 +0100

    using serilog log context mask sensitive data in logs

---
+++ UserManagementService.Api/Logging/SerilogSafeDestructureExtension.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using UserManagementService.Core.DTOs;
+
+namespace UserManagementService.Api.Logging;
+
+public static class SerilogSafeDestructureExtensions
+{
+    public static LoggerConfiguration DestructureSensitiveDtos(this LoggerConfiguration config)
+    {
+        return config
+            .Destructure.ByTransforming<CreateUserRequest>(r => new
+            {
+                r.UserName,
+                r.FullName,
+                r.Email,
+                r.MobileNumber,
+                r.Language,
+                r.Culture,
+                Password = "***"
+            })
+            .Destructure.ByTransforming<ValidatePasswordRequest>(r => new
+            {
+                Password = "***"
+            });
+    }
+}
